fbde556d829e6413ba8ff77d80b3b8ec2308c5b6
Boo <aurelieboo1219@gmail.com>
2021-01-18 09:29:12 +0100

    class vilain

---
+++ src/classes/Vilain.php
@@ -0,0 +1,35 @@
+<?php
+
+class Vilain {
+
+    private $_id;
+    private $_name;
+    private $_xp;
+    private $_weapon;
+    private $_health;
+    private $_strength;
+    private $_stamina;
+    private $_bitcoin;
+
+    public function __construct()
+    {
+
+    }
+
+    public function fight()
+    {
+
+    }
+
+    public function alive()
+    {
+
+    }
+
+    public function win()
+    {
+
+    }
+
+
+}
