fb4f738dc81fde80b38c74e4f5909ac94edf131a
MJ-Brusk <mjebriell@gmail.com>
2021-01-18 09:46:30 +0100

    Weapon

---
+++ src/classes/Weapon.php
@@ -0,0 +1,38 @@
+<?php
+
+class Weapon
+ {
+     // Code starts here...
+
+
+     // Properties
+     private $_id;
+     private $_name;
+     private $_strength;
+     private $_stamina;
+     private $_price;
+
+    public function __construct() {}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+?>
