f01f829ddba4a7224b2235fe76ad4fc230bc73f5
aoumar <aoumar.lahouari@gmail.com>
2021-01-20 16:10:56 +0100

    création escape.php

---
+++ src/action/escape.php
@@ -0,0 +1,23 @@
+<?php
+// création du lien avec Base De Données
+require '../src/config/database.php';
+
+ //declaration de variable query
+ $query = $pdo->query("SELECT * FROM Hero");
+
+ $Hero = $query->fetch();
+
+ $xp = $Hero->xp;
+
+//Test conditionnel si xp > 1
+
+ if ($xp >= 1){
+
+     $xp = $Hero->xp-1 ;
+
+     $req = $pdo->query("UPDATE Hero SET xp = $xp");
+     
+ }
+header ("location: ../src/action/nextPage.php?page=' . $_GET[\'page\'] . '&id=' . $_GET[\'id\']'");
+
+?>
