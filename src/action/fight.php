1d49d6e45c926bd93c541a08fb3d17da3a19693d
Axel Duret <axelfaya@gmail.com>
2021-01-21 12:14:07 +0100

    add fight method

---
+++ src/action/fight.php
@@ -0,0 +1,69 @@
+<?php
+
+// création du lien avec Base De Données
+require '../config/database.php';
+
+//Declaration de variables pour redirection de page
+$id = $_GET["id"];
+
+// On recherche la table Hero
+$query = $pdo->query("SELECT * FROM Hero");
+$hero = $query->fetch();
+$idHero = $hero->id;
+
+// On recherche l'id de l'arme équipée par le Hero
+$query3 = $pdo->query("SELECT idWeapon FROM heroWeapon WHERE idHero = $idHero AND gear = 1");
+$heroWeapon = $query3->fetch();
+$weaponId = $heroWeapon->idWeapon;
+
+// On recherche le nom de l'arme équipée par le Hero
+$query4 = $pdo->query("SELECT * FROM Weapon WHERE id = $weaponId");
+$weaponStat = $query4->fetch();
+
+// On recherche les info du Vilain correspondant à son id
+$query2 = $pdo->query("SELECT * FROM Vilain WHERE id = $id");
+$vilain = $query2->fetch();
+
+$vilainStrength = $vilain->strength;
+$vilainStamina = $vilain->stamina;
+$vilainHealth = $vilain->health;
+$vilainPower = $vilainStrength + $vilainStamina;
+$weaponStrength = $weaponStat->strength;
+$weaponStamina = $weaponStat->stamina;
+$heroStrength = $hero->strength;
+$heroStamina = $hero->stamina;
+$heroHealth = $hero->health;
+$heroPower = $heroStrength + $heroStamina + $weaponStrength + $weaponStamina;
+
+if ($vilainPower >= $heroPower) {
+
+    $result1 = $vilainPower - $heroPower;
+    $heroPv = $heroHealth - $result1;
+
+    $req = $pdo->query("UPDATE Hero SET health = $heroPv");
+} elseif ($heroPower > $vilainPower) {
+
+    $result2 = $heroPower - $vilainPower;
+    $vilainPv = $vilainHealth - $result2;
+    $heroXp = $hero->xp + 1;
+
+    $req2 = $pdo->query("UPDATE Vilain SET health = $vilainPv WHERE id = $id");
+
+    $req3 = $pdo->query("UPDATE Hero SET xp = $heroXp");
+
+    $query2 = $pdo->query("SELECT * FROM Vilain WHERE id = $id");
+    $vilain = $query2->fetch();
+
+
+    $vilainHealth = $vilain->health;
+
+    if ($vilainHealth <= 0) {
+
+        $pdo->query("DELETE FROM Vilain WHERE id = $id");
+    }
+}
+
+
+
+
+header("location: nextPage.php");
